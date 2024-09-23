4f15ed4094993cd9aaba7e693e5a4ef9ff0047e3
Rfblaze <angelotroncone@gmail.com>
2024-09-23 14:43:34 -0600

    main code structure works; sierpinski does not

--- 453-skeleton/main.cpp
+++ 453-skeleton/main.cpp
@@ -10,18 +10,6 @@
 #include "Shader.h"
 #include "Window.h"
 
-struct Parameters {
-	const float tStep = 0.1f;
-	const float uStep = 0.1f;
-
-	float t = 1.0f;
-	float u = 0.0f;
-
-	bool isDifferent(Parameters p){
-		return p.t != t || p.u != u;
-	}
-};
-
 // EXAMPLE CALLBACKS
 class MyCallbacks : public CallbackInterface {
 
@@ -29,9 +17,9 @@ public:
 	MyCallbacks(ShaderProgram& shader) : shader(shader) {}
 
 	virtual void keyCallback(int key, int scancode, int action, int mods) {
-		// if (key == GLFW_KEY_R && action == GLFW_PRESS) {
-		// 	shader.recompile();
-		// }
+		if (key == GLFW_KEY_R && action == GLFW_PRESS) {
+			shader.recompile();
+		}
 		
 		if (action == GLFW_PRESS || action == GLFW_REPEAT){
 			if (key == GLFW_KEY_R && action == GLFW_PRESS) {
@@ -39,57 +27,120 @@ public:
 			}
 
 			if (key == GLFW_KEY_UP) {
-				parameters.t += parameters.tStep;
-				std::cout << "t:" << parameters.t << std::endl;
+				depth_n += 1;
 			}
 
 			if (key == GLFW_KEY_DOWN) {
-				parameters.t -= parameters.tStep;
-				std::cout << "t:" << parameters.t << std::endl;
+				if (depth_n > 0){
+					depth_n -= 1;
+				}
 			}
 
 			if (key == GLFW_KEY_RIGHT) {
-				parameters.u += parameters.uStep;
-				std::cout << "u:" << parameters.u << std::endl;
+				scene += 1;
 			}
 
 			if (key == GLFW_KEY_LEFT) {
-				parameters.u -= parameters.uStep;
-				std::cout << "u:" << parameters.u << std::endl;
+				if (scene > 1){
+					scene -= 1;
+				}
 			}
 		}
 	}
 
-	Parameters getParameters(){
-		return parameters;
+	std::vector<int> getParameters(){
+		return std::vector<int>{depth_n, scene};
 	}
+
+	
 
 private:
 	ShaderProgram& shader;
-	Parameters parameters;
+	int depth_n = 0;
+	int scene = 1;
+
 };
 
-// class MyCallbacks2 : public CallbackInterface {
+CPU_Geometry sierpinski_triangle(glm::vec3 p1, glm::vec3 p2, glm::vec3 p3, int depth_n, bool base_triangle){
+	CPU_Geometry cpugeom;
+	
+	if (depth_n > 0){
+		glm::vec3 q1((0.5 * p1.x) + (0.5 * p3.x), (0.5 * p1.y) + (0.5 * p3.y), 0.f);
+		glm::vec3 q2((0.5 * p1.x) + (0.5 * p2.x), (0.5 * p1.y) + (0.5 * p2.y), 0.f);
+		glm::vec3 q3((0.5 * p2.x) + (0.5 * p3.x), (0.5 * p2.y) + (0.5 * p3.y), 0.f);
 
-// public:
-// 	MyCallbacks2() {}
+		// draw black triangle
+		cpugeom.verts.push_back(q1);
+		cpugeom.verts.push_back(q2);
+		cpugeom.verts.push_back(q3);
 
-// 	virtual void keyCallback(int key, int scancode, int action, int mods) {
-// 		if (key == GLFW_KEY_R && action == GLFW_PRESS) {
-// 			std::cout << "called back" << std::endl;
-// 		}
-// 	}
-// };
-// END EXAMPLES
+		cpugeom.cols.push_back(glm::vec3(0.f, 0.f,0.f));
+		cpugeom.cols.push_back(glm::vec3(0.f, 0.f,0.f));
+		cpugeom.cols.push_back(glm::vec3(0.f, 0.f,0.f));
 
-CPU_Geometry generateSin(Parameters p){
-	CPU_Geometry cpuGeom;
-	for (float x = -1.0f; x <= 1.0f; x+=0.01f){
-		cpuGeom.verts.push_back(glm::vec3(x, p.u * sin(x * p.t), 0.f));
-		cpuGeom.cols.push_back(glm::vec3(1.f, 0.f, 0.f));
+
+		sierpinski_triangle(p1, q2, q3, depth_n - 1, false);
+		sierpinski_triangle(q2, p2, q3, depth_n - 1, false);
+		sierpinski_triangle(q1, q3, p3, depth_n - 1, false);
+
+
 	}
-	return cpuGeom;
+	else if (depth_n == 0 && base_triangle){
+		// draw colored triangle, add new points to cpugeom
+		cpugeom.verts.push_back(glm::vec3(-0.5f, -0.5f, 0.f));
+		cpugeom.verts.push_back(glm::vec3(0.5f, -0.5f, 0.f));
+		cpugeom.verts.push_back(glm::vec3(0.f, 0.5f, 0.f));
+
+		cpugeom.cols.push_back(glm::vec3(1.f, 0.f, 0.f));
+		cpugeom.cols.push_back(glm::vec3(1.f, 0.f, 0.f)); 
+		cpugeom.cols.push_back(glm::vec3(1.f, 0.f, 0.f)); 
+
+	}
+	return cpugeom;
 }
+
+CPU_Geometry pythagoras_tree(){
+	CPU_Geometry cpugeom;
+
+	cpugeom.verts.push_back(glm::vec3(-0.5f, -0.5f, 0.f));
+	cpugeom.verts.push_back(glm::vec3(0.5f, -0.5f, 0.f));
+	cpugeom.verts.push_back(glm::vec3(0.f, 0.5f, 0.f));
+
+	cpugeom.cols.push_back(glm::vec3(1.f, 0.f, 0.f));
+	cpugeom.cols.push_back(glm::vec3(1.f, 0.f, 0.f)); 
+	cpugeom.cols.push_back(glm::vec3(1.f, 0.f, 0.f)); 
+
+	return cpugeom;
+}
+
+CPU_Geometry koch_snowflake(){
+	CPU_Geometry cpugeom;
+	
+	cpugeom.verts.push_back(glm::vec3(-0.5f, -0.5f, 0.f));
+	cpugeom.verts.push_back(glm::vec3(0.5f, -0.5f, 0.f));
+	cpugeom.verts.push_back(glm::vec3(0.f, 0.5f, 0.f));
+
+	cpugeom.cols.push_back(glm::vec3(1.f, 0.f, 0.f));
+	cpugeom.cols.push_back(glm::vec3(1.f, 0.f, 0.f)); 
+	cpugeom.cols.push_back(glm::vec3(1.f, 0.f, 0.f)); 
+
+	return cpugeom;
+}
+
+CPU_Geometry dragon_curve(){
+	CPU_Geometry cpugeom;
+	
+	cpugeom.verts.push_back(glm::vec3(-0.5f, -0.5f, 0.f));
+	cpugeom.verts.push_back(glm::vec3(0.5f, -0.5f, 0.f));
+	cpugeom.verts.push_back(glm::vec3(0.f, 0.5f, 0.f));
+
+	cpugeom.cols.push_back(glm::vec3(1.f, 0.f, 0.f));
+	cpugeom.cols.push_back(glm::vec3(1.f, 0.f, 0.f)); 
+	cpugeom.cols.push_back(glm::vec3(1.f, 0.f, 0.f)); 
+
+	return cpugeom;
+}
+
 
 int main() {
 	Log::debug("Starting main");
@@ -110,44 +161,55 @@ int main() {
 	// GEOMETRY
 	CPU_Geometry cpuGeom;
 	GPU_Geometry gpuGeom;
-	
-	Parameters p;
-	cpuGeom = generateSin(p);
-	
-
-	// // vertices
-	// cpuGeom.verts.push_back(glm::vec3(-0.5f, -0.5f, 0.f));
-	// cpuGeom.verts.push_back(glm::vec3(0.5f, -0.5f, 0.f));
-	// cpuGeom.verts.push_back(glm::vec3(0.f, 0.5f, 0.f));
-
-	// // colours (these should be in linear space)
-	// cpuGeom.cols.push_back(glm::vec3(1.f, 0.f, 0.f)); // red
-	// cpuGeom.cols.push_back(glm::vec3(0.f, 1.f, 0.f)); // green
-	// cpuGeom.cols.push_back(glm::vec3(0.f, 0.f, 1.f)); // blue
-
-	gpuGeom.setVerts(cpuGeom.verts);
-	gpuGeom.setCols(cpuGeom.cols);
 
 	// RENDER LOOP
 	while (!window.shouldClose()) {
 		glfwPollEvents();
 
-		Parameters newP = callbacks->getParameters();
+		std::vector<int> user_input = callbacks->getParameters();
+		
+		switch (user_input[1]){
+			case 1:
+				cpuGeom = sierpinski_triangle(
+					glm::vec3(-0.5f, -0.5f, 0.f), 
+					glm::vec3(0.5f, -0.5f, 0.f), 
+					glm::vec3(0.f, 0.5f, 0.f), 
+					user_input[0], 
+					true
+				);
 
-		if (newP.isDifferent(p)){
-			cpuGeom = generateSin(newP);
-			gpuGeom.setVerts(cpuGeom.verts);
-			gpuGeom.setCols(cpuGeom.cols);
+				// cpuGeom.verts.push_back(glm::vec3(-0.5f, -0.5f, 0.f));
+				// cpuGeom.verts.push_back(glm::vec3(0.5f, -0.5f, 0.f));
+				// cpuGeom.verts.push_back(glm::vec3(0.f, 0.5f, 0.f));
+
+				// cpuGeom.cols.push_back(glm::vec3(1.f, 0.f, 0.f));
+				// cpuGeom.cols.push_back(glm::vec3(1.f, 0.f, 0.f)); 
+				// cpuGeom.cols.push_back(glm::vec3(1.f, 0.f, 0.f)); 
+
+				gpuGeom.setCols(cpuGeom.cols);
+				gpuGeom.setVerts(cpuGeom.verts);
+
+				shader.use();
+				gpuGeom.bind();
+
+				glEnable(GL_FRAMEBUFFER_SRGB);
+				glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+				glDrawArrays(GL_TRIANGLES, 0, GLsizei(cpuGeom.verts.size()));
+				glDisable(GL_FRAMEBUFFER_SRGB); // disable sRGB for things like imgui
+				
+				break;
+			case 2:
+				cpuGeom = pythagoras_tree();
+				break;
+			case 3:
+				cpuGeom = koch_snowflake();
+				break;
+			case 4:
+				cpuGeom = dragon_curve();
+				break;
 		}
-
-		shader.use();
-		gpuGeom.bind();
-
-		glEnable(GL_FRAMEBUFFER_SRGB);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		// glDrawArrays(GL_TRIANGLES, 0, 3);
-		glDrawArrays(GL_LINE_STRIP, 0, GLsizei(cpuGeom.verts.size()));
-		glDisable(GL_FRAMEBUFFER_SRGB); // disable sRGB for things like imgui
+		
+		
 
 		window.swapBuffers();
 	}
