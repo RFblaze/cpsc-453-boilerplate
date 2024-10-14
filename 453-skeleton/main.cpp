2fa35254ea4a82a431ab8c58a9edd629b6580672
Rfblaze <angelotroncone@gmail.com>
2024-10-14 12:18:43 -0600

    Fixed ship not displaying when update called; rotates weirdly

--- 453-skeleton/main.cpp
+++ 453-skeleton/main.cpp
@@ -17,7 +17,7 @@
 #include "imgui/imgui_impl_opengl3.h"
 #include <glm/gtc/type_ptr.hpp>
 
-CPU_Geometry shipGeom(float width, float height, glm::vec3 offset, float angle);
+CPU_Geometry shipGeom(float scale, glm::vec3 center, glm::vec3 facing);
 
 // An example struct for Game Objects.
 // You are encouraged to customize this as you see fit.
@@ -29,8 +29,24 @@ struct GameObject {
 		position(0.0f, 0.0f, 0.0f),
 		facing(0.f, 1.f, 0.f),
 		scale(1),
-		transformationMatrix(1.0f) // This constructor sets it as the identity matrix
-	{}
+		transformationMatrix(1.0f)// This constructor sets it as the identity matrix
+	{
+		// Set default size
+		cgeom.verts.push_back(glm::vec3(-0.09f, 0.09f, 0.f));
+		cgeom.verts.push_back(glm::vec3(-0.09f, -0.09f, 0.f));
+		cgeom.verts.push_back(glm::vec3(0.09f, -0.09f, 0.f));
+		cgeom.verts.push_back(glm::vec3(-0.09f, 0.09f, 0.f));
+		cgeom.verts.push_back(glm::vec3(0.09f, -0.09f, 0.f));
+		cgeom.verts.push_back(glm::vec3(0.09f, 0.09f, 0.f));
+
+		cgeom.texCoords.push_back(glm::vec2(0.f, 1.f));
+		cgeom.texCoords.push_back(glm::vec2(0.f, 0.f));
+		cgeom.texCoords.push_back(glm::vec2(1.f, 0.f));
+		cgeom.texCoords.push_back(glm::vec2(0.f, 1.f));
+		cgeom.texCoords.push_back(glm::vec2(1.f, 0.f));
+		cgeom.texCoords.push_back(glm::vec2(1.f, 1.f));
+
+	}
 
 	CPU_Geometry cgeom;
 	GPU_Geometry ggeom;
@@ -42,19 +58,42 @@ struct GameObject {
 	glm::mat4 transformationMatrix;
 
 	// Just testing to see if movement works
-	CPU_Geometry update(glm::vec3 displacement, glm::vec3 direction){
-		this->position = displacement;
-		glm::vec3 newFacing = (this->facing + direction) / glm::length(this->facing + direction);
+	void update(glm::vec3 displacement, float direction){
+		CPU_Geometry newGeom;
 
-		float angle = glm::dot(this->facing, newFacing) / (glm::length(this->facing) * glm::length(newFacing));
+		// if ship would move out of bounds
+		bool goingOutOfBounds = false;
+		if (!goingOutOfBounds){
+			this->position += displacement;
+		}
 
-		return shipGeom(0.18f, 0.18f, this->position, angle);
+		glm::mat4 rotationMatrix = glm::rotate(glm::mat4(1.f), glm::radians(direction), glm::vec3(0.f, 0.f, 1.f));
+    	glm::vec3 newfacing = glm::vec3(rotationMatrix * glm::vec4(this->facing, 0.0f));
+
+		this->facing = newfacing;
+
+		std::cout << "Position: " << glm::to_string(this->position) << std::endl;
+		std::cout << "Facing: " << glm::to_string(this->facing) << std::endl;
+
+
+		newGeom = shipGeom(this->scale, this->position, this->facing);
+		
+		// add the newGeom verts to the GameObject by first deleting the old ones
+		// no need to add the texture coords since they don't change
+
+		this->cgeom.verts.clear();
+		for (int i = 0; i < 6; i++){
+			this->cgeom.verts.push_back(newGeom.verts[i]);
+		}
+
+		this->ggeom.setVerts(this->cgeom.verts);
+		this->ggeom.setTexCoords(this->cgeom.texCoords);
 	}
 };
 
 struct Parameters{
 	glm::vec3 displacement = {0.f,0.f,0.f};
-	glm::vec3 direction = {0.f,0.f,0.f};
+	float direction = 0.f;
 };
 
 // EXAMPLE CALLBACKS
@@ -74,7 +113,7 @@ public:
 			}
 
 			if (key == GLFW_KEY_A){
-				playerInputs.direction += glm::vec3(-1.f, 0.f, 0.f);
+				playerInputs.direction -= 5.f;
 			}
 
 			if (key == GLFW_KEY_S){
@@ -82,14 +121,15 @@ public:
 			}
 
 			if (key == GLFW_KEY_D){
-				playerInputs.direction += glm::vec3(1.f, 0.f, 0.f);
+				playerInputs.direction += 5.f;
 			}
 		}
 	}
 
 	Parameters getParameters(){
 		Parameters ret = playerInputs;
-
+		playerInputs.direction = 0.f;
+		playerInputs.displacement = glm::vec3(0.f,0.f,0.f);
 		return ret;
 	}
 
@@ -98,32 +138,44 @@ private:
 	Parameters playerInputs;
 };
 
-CPU_Geometry shipGeom(float width, float height, glm::vec3 offset, float angle) {
+CPU_Geometry shipGeom(float scale, glm::vec3 center, glm::vec3 facing) {
 	CPU_Geometry retGeom;
 
-	float halfWidth = width / 2.0f;
-	float halfHeight = height / 2.0f;
+	// default width & height = 0.18f
 
-	retGeom.verts.push_back(glm::vec3(-halfWidth + offset.x, halfHeight + offset.y, 0.f));
-	retGeom.verts.push_back(glm::vec3(-halfWidth + offset.x, -halfHeight + offset.y, 0.f));
-	retGeom.verts.push_back(glm::vec3(halfWidth + offset.x, -halfHeight + offset.y, 0.f));
-	retGeom.verts.push_back(glm::vec3(-halfWidth + offset.x, halfHeight + offset.y, 0.f));
-	retGeom.verts.push_back(glm::vec3(halfWidth + offset.x, -halfHeight + offset.y, 0.f));
-	retGeom.verts.push_back(glm::vec3(halfWidth + offset.x, halfHeight + offset.y, 0.f));
+	float halfWidth = 0.18f * 0.5f * scale; 
+	float halfHeight = 0.18f * 0.5f * scale;
 
-	// This rotates them based on angle
-	for (size_t i = 0; i < retGeom.verts.size(); i++){
-		float x = retGeom.verts[i].x;
-		float y = retGeom.verts[i].y;
+	// rotation angle wrt y-axis
+	float angle = glm::dot(glm::vec3(0.f,1.f,0.f), facing);
 
-		// Apply the 2D rotation formula
-        float newX = x * cos(angle) - y * sin(angle);
-        float newY = x * sin(angle) + y * cos(angle);
+	// vector from origin to the 4 corners of ship
+	glm::vec3 v0 = glm::vec3(-halfWidth, halfHeight,0.f) - glm::vec3(0.f,0.f,0.f);
+	glm::vec3 v1 = glm::vec3(-halfWidth, -halfHeight,0.f) - glm::vec3(0.f,0.f,0.f);
+	glm::vec3 v2 = glm::vec3(halfWidth, -halfHeight,0.f) - glm::vec3(0.f,0.f,0.f);
+	glm::vec3 v3 = glm::vec3(halfWidth, halfHeight,0.f) - glm::vec3(0.f,0.f,0.f);
 
-        // Update the vertex position
-        retGeom.verts[i].x = newX;
-        retGeom.verts[i].y = newY;
-	}
+	// rotate the vectors by the rotation angle
+	glm::mat4 rotationMatrix = glm::rotate(glm::mat4(1.f), glm::radians(angle), glm::vec3(0.f, 0.f, 1.f));
+
+	glm::vec3 v0prime = glm::vec3(rotationMatrix * glm::vec4(v0, 0.0f));
+	glm::vec3 v1prime = glm::vec3(rotationMatrix * glm::vec4(v1, 0.0f));
+	glm::vec3 v2prime = glm::vec3(rotationMatrix * glm::vec4(v2, 0.0f));
+	glm::vec3 v3prime = glm::vec3(rotationMatrix * glm::vec4(v3, 0.0f));
+
+	// get the point of the corners of the ship
+	glm::vec3 p0 = center + v0prime;
+	glm::vec3 p1 = center + v1prime;
+	glm::vec3 p2 = center + v2prime;
+	glm::vec3 p3 = center + v3prime;
+
+	retGeom.verts.push_back(p0);
+	retGeom.verts.push_back(p1);
+	retGeom.verts.push_back(p2);
+	retGeom.verts.push_back(p0);
+	retGeom.verts.push_back(p2);
+	retGeom.verts.push_back(p3);
+	
 	// vertices for the spaceship quad
 	// For full marks (Part IV), you'll need to use the following vertex coordinates instead.
 	// Then, you'd get the correct scale/translation/rotation by passing in uniforms into
@@ -144,6 +196,15 @@ CPU_Geometry shipGeom(float width, float height, glm::vec3 offset, float angle) 
 	retGeom.texCoords.push_back(glm::vec2(0.f, 1.f));
 	retGeom.texCoords.push_back(glm::vec2(1.f, 0.f));
 	retGeom.texCoords.push_back(glm::vec2(1.f, 1.f));
+
+	std::cout << "Vertices:" << std::endl;
+	std::cout << glm::to_string(retGeom.verts[0]) << std::endl;
+	std::cout << glm::to_string(retGeom.verts[1]) << std::endl;
+	std::cout << glm::to_string(retGeom.verts[2]) << std::endl;
+	std::cout << glm::to_string(retGeom.verts[3]) << std::endl;
+	std::cout << glm::to_string(retGeom.verts[4]) << std::endl;
+	std::cout << glm::to_string(retGeom.verts[5]) << std::endl;
+
 	return retGeom;
 }
 
@@ -193,22 +254,8 @@ int main() {
 	GameObject ship("textures/ship.png", GL_NEAREST);
 	GameObject diamond("textures/diamond.png", GL_NEAREST);
 
-	ship.cgeom = shipGeom(0.18f, 0.18f, glm::vec3(0.f,0.f,0.f), 0.f);
-
-	// translation
-	glm::mat4 M = glm::translate(glm::mat4(1.f), glm::vec3(0.1f, 0.1f, 0.f));
-
-	for(glm::vec3& vert : ship.cgeom.verts){
-		vert = M * glm::vec4(vert, 1.f);
-	}
-
-	diamond.cgeom = diamondGeom();
-
-
-	ship.ggeom.setVerts(ship.cgeom.verts);
-	ship.ggeom.setTexCoords(ship.cgeom.texCoords);
-	diamond.ggeom.setVerts(diamond.cgeom.verts);
-	diamond.ggeom.setTexCoords(diamond.cgeom.texCoords);
+	glm::vec3 curr_direction = ship.facing;
+	glm::vec3 curr_position = ship.position;
 
 	// RENDER LOOP
 	while (!window.shouldClose()) {
@@ -224,11 +271,14 @@ int main() {
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		// Get the user input details
-		// glm::vec3 direction_change = user_input.direction;
-		// glm::vec3 displacement_change = user_input.displacement;
+		float direction_change = user_input.direction;
+		glm::vec3 displacement_change = user_input.displacement;
+
 
 		// Recalculate ship position
-		// ship.cgeom = ship.update(displacement_change, direction_change);
+		if (direction_change != 0.f || !(displacement_change[0] == 0.f && displacement_change[1] == 0.f && displacement_change[2] == 0.f)){
+			ship.update(displacement_change, direction_change);
+		}
 
 		ship.ggeom.setVerts(ship.cgeom.verts);
 		ship.ggeom.setTexCoords(ship.cgeom.texCoords);
@@ -240,13 +290,15 @@ int main() {
 		ship.texture.bind();
 
 		// Here go the transformations
-		glUniformMatrix4fv(0, 1, GL_FALSE, glm::value_ptr(M));
+		/*
+		
+		*/
 		
 		
 		// Draw ship then unbind texture
 		glDrawArrays(GL_TRIANGLES, 0, 6);
 		
-		// ship.texture.unbind();
+		ship.texture.unbind();
 
 		// // Then render the diamond
 		// diamond.ggeom.bind();
