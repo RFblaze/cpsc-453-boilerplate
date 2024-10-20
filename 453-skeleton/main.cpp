ddd1c1611fd8b69122366898ff30c9e20da9f8eb
Rfblaze <angelotroncone@gmail.com>
2024-10-19 21:39:23 -0600

    Diamond movement works but boundaries don't

--- 453-skeleton/main.cpp
+++ 453-skeleton/main.cpp
@@ -17,19 +17,19 @@
 #include "imgui/imgui_impl_opengl3.h"
 #include <glm/gtc/type_ptr.hpp>
 
-CPU_Geometry shipGeom(float scale, glm::vec3 center, glm::vec3 facing);
+struct Parameters{
+	glm::vec3 displacement = {0.f,0.f,0.f};
+	glm::vec3 cursorPos = {0.f,0.f,0.f};
+};
 
 // An example struct for Game Objects.
 // You are encouraged to customize this as you see fit.
 struct GameObject {
-	// Struct's constructor deals with the texture.
-	// Also sets default position, theta, scale, and transformationMatrix
+	// Ship Constructor
 	GameObject(std::string texturePath, GLenum textureInterpolation) :
 		texture(texturePath, textureInterpolation),
 		position(0.0f, 0.0f, 0.0f),
-		facing(0.f, 1.f, 0.f),
-		scale(1),
-		transformationMatrix(1.0f)// This constructor sets it as the identity matrix
+		facing(0.f, 1.f, 0.f)
 	{
 		
 		cgeom.verts.push_back(glm::vec3(-1.f, 1.f, 0.f));
@@ -46,27 +46,100 @@ struct GameObject {
 		cgeom.texCoords.push_back(glm::vec2(1.f, 0.f));
 		cgeom.texCoords.push_back(glm::vec2(1.f, 1.f));
 
+		S_Matrix = glm::scale(glm::mat4(1.f), glm::vec3(default_ship_size, default_ship_size, 0.f));
+		T_Matrix = glm::translate(glm::mat4(1.f), glm::vec3(0.f,0.f,0.f));
+		R_matrix = glm::rotate(glm::mat4(1.f), glm::radians(0.f), glm::vec3(0.f,0.f,1.f));
+
+	}
+
+	// Diamond Constructor
+	// FIXME: pos must be set to the T_Matrix
+	GameObject(std::string texturePath, GLenum textureInterpolation, glm::vec3 pos, glm::vec3 face) :
+		texture(texturePath, textureInterpolation),
+		position(pos),
+		facing(face)
+	{
+		
+		cgeom.verts.push_back(glm::vec3(-1.f, 1.f, 0.f));
+		cgeom.verts.push_back(glm::vec3(-1.f, -1.f, 0.f));
+		cgeom.verts.push_back(glm::vec3(1.f, -1.f, 0.f));
+		cgeom.verts.push_back(glm::vec3(-1.f, 1.f, 0.f));
+		cgeom.verts.push_back(glm::vec3(1.f, -1.f, 0.f));
+		cgeom.verts.push_back(glm::vec3(1.f, 1.f, 0.f));
+
+		cgeom.texCoords.push_back(glm::vec2(0.f, 1.f));
+		cgeom.texCoords.push_back(glm::vec2(0.f, 0.f));
+		cgeom.texCoords.push_back(glm::vec2(1.f, 0.f));
+		cgeom.texCoords.push_back(glm::vec2(0.f, 1.f));
+		cgeom.texCoords.push_back(glm::vec2(1.f, 0.f));
+		cgeom.texCoords.push_back(glm::vec2(1.f, 1.f));
+
+		S_Matrix = glm::scale(glm::mat4(1.f), glm::vec3(default_diamond_size, default_diamond_size, 0.f));
+		T_Matrix = glm::translate(glm::mat4(1.f), glm::vec3(0.f,0.f,0.f));
+		R_matrix = glm::rotate(glm::mat4(1.f), glm::radians(0.f), glm::vec3(0.f,0.f,1.f));
 	}
 
 	CPU_Geometry cgeom;
 	GPU_Geometry ggeom;
 	Texture texture;
 
+	float default_ship_size = 0.18f;
+	float default_diamond_size = 0.18f;
+
 	glm::vec3 position;
 	glm::vec3 facing;
-	float scale; // Or, alternatively, a glm::vec2 scale;
-	glm::mat4 transformationMatrix;
+	glm::mat4 S_Matrix;
+	glm::mat4 R_matrix;
+	glm::mat4 T_Matrix;
+	
 
-	// Just testing to see if movement works
-	void update(glm::vec3 displacement, float direction){
+	glm::mat4 getTransformationMatrix(){
+		return T_Matrix * R_matrix * S_Matrix;
+	}
+
+	void updateShip(Parameters user_input, int counter){
+		// Compute changes
+		glm::vec3 mustFace = user_input.cursorPos - this->position;
+		glm::vec3 face_uvector = mustFace / float(mustFace.length());
+
+		// Compute the change in position in the direction of the ship's current facing vector
+		glm::vec3 displacement = user_input.displacement.y * this->facing; // Move in the facing direction
+
+		// Update position
+		this->position += displacement;
+
+		float angle = acos(glm::dot(this->facing, face_uvector));
+
+		glm::vec3 posChange = this->position + (face_uvector * user_input.displacement);
+
+
+		// Apply changes to ship object data members
+		this->facing = face_uvector;
+
+		// Change the transformation matrices
+		glm::mat4 added_S = glm::scale(glm::mat4(1.f), glm::vec3(1.f + counter * 0.25, 1.f + counter * 0.25, 1.f));
+		this->S_Matrix = added_S * this->S_Matrix;
+
+		glm::mat4 added_R = glm::rotate(glm::mat4(1.f), glm::radians(angle), glm::vec3(0.f,0.f,1.f));
+		this->R_matrix = added_R * this->R_matrix;
+
+		glm::mat4 added_T = glm::translate(glm::mat4(1.0f), posChange);
+		this->T_Matrix = added_T * this->T_Matrix;
 		
 	}
-};
 
-struct Parameters{
-	glm::vec3 displacement = {0.f,0.f,0.f};
-	float direction = 0.f;
-	glm::vec2 cursor = {0.f,0.f};
+	void updateDiamondPosition(){
+		// check if going out of bounds
+		if (abs((this->position + this->facing).x) >= 1.0f || abs((this->position + this->facing).y) >= 1.0f){
+			this->facing = -this->facing;
+		}
+		glm::vec3 displacement = this->facing * 0.015f;
+		this->position += displacement;
+
+		glm::mat4 added_T = glm::translate(glm::mat4(1.f), displacement);
+		this->T_Matrix = added_T * this->T_Matrix;
+	}
+
 };
 
 // EXAMPLE CALLBACKS
@@ -120,84 +193,18 @@ public:
 
 		std::cout << cursor.x << " " << cursor.y << std::endl;
 
-		cursorPos.x = cursor.x;
-		cursorPos.y = cursor.y;
-
-		// Add direction logic set
+		playerInputs.cursorPos.x = cursor.x;
+		playerInputs.cursorPos.y = cursor.y;
 	}
 
 	Parameters getParameters(){
-		Parameters ret = playerInputs;
-		playerInputs.direction = 0.f;
-		playerInputs.displacement = glm::vec3(0.f,0.f,0.f);
-		return ret;
+		return playerInputs;
 	}
 
 private:
 	ShaderProgram& shader;
 	Parameters playerInputs;
-
-	glm::vec2 cursorPos;
 };
-
-CPU_Geometry shipGeom() {
-	CPU_Geometry retGeom;
-	
-	// vertices for the spaceship quad
-	// For full marks (Part IV), you'll need to use the following vertex coordinates instead.
-	// Then, you'd get the correct scale/translation/rotation by passing in uniforms into
-	// the vertex shader.
-	
-	retGeom.verts.push_back(glm::vec3(-1.f, 1.f, 0.f));
-	retGeom.verts.push_back(glm::vec3(-1.f, -1.f, 0.f));
-	retGeom.verts.push_back(glm::vec3(1.f, -1.f, 0.f));
-	retGeom.verts.push_back(glm::vec3(-1.f, 1.f, 0.f));
-	retGeom.verts.push_back(glm::vec3(1.f, -1.f, 0.f));
-	retGeom.verts.push_back(glm::vec3(1.f, 1.f, 0.f));
-	
-
-	// texture coordinates
-	retGeom.texCoords.push_back(glm::vec2(0.f, 1.f));
-	retGeom.texCoords.push_back(glm::vec2(0.f, 0.f));
-	retGeom.texCoords.push_back(glm::vec2(1.f, 0.f));
-	retGeom.texCoords.push_back(glm::vec2(0.f, 1.f));
-	retGeom.texCoords.push_back(glm::vec2(1.f, 0.f));
-	retGeom.texCoords.push_back(glm::vec2(1.f, 1.f));
-
-	std::cout << "Vertices:" << std::endl;
-	std::cout << glm::to_string(retGeom.verts[0]) << std::endl;
-	std::cout << glm::to_string(retGeom.verts[1]) << std::endl;
-	std::cout << glm::to_string(retGeom.verts[2]) << std::endl;
-	std::cout << glm::to_string(retGeom.verts[3]) << std::endl;
-	std::cout << glm::to_string(retGeom.verts[4]) << std::endl;
-	std::cout << glm::to_string(retGeom.verts[5]) << std::endl;
-
-	return retGeom;
-}
-
-CPU_Geometry diamondGeom(){
-	CPU_Geometry retGeom;
-
-	// Square is made of two triangles, so 6 vertices
-	retGeom.verts.push_back(glm::vec3(-1.f, 1.f, 0.f));
-	retGeom.verts.push_back(glm::vec3(-1.f, -1.f, 0.f));
-	retGeom.verts.push_back(glm::vec3(1.f, -1.f, 0.f));
-	retGeom.verts.push_back(glm::vec3(-1.f, 1.f, 0.f));
-	retGeom.verts.push_back(glm::vec3(1.f, -1.f, 0.f));
-	retGeom.verts.push_back(glm::vec3(1.f, 1.f, 0.f));
-
-
-	retGeom.texCoords.push_back(glm::vec2(0.f, 1.f));
-	retGeom.texCoords.push_back(glm::vec2(0.f, 0.f));
-	retGeom.texCoords.push_back(glm::vec2(1.f, 0.f));
-	retGeom.texCoords.push_back(glm::vec2(0.f, 1.f));
-	retGeom.texCoords.push_back(glm::vec2(1.f, 0.f));
-	retGeom.texCoords.push_back(glm::vec2(1.f, 1.f));
-
-	return retGeom;
-}
-
-// END EXAMPLES
 
 int main() {
 	Log::debug("Starting main");
@@ -219,21 +226,26 @@ int main() {
 	// GL_NEAREST looks a bit better for low-res pixel art than GL_LINEAR.
 	// But for most other cases, you'd want GL_LINEAR interpolation.
 	GameObject ship("textures/ship.png", GL_NEAREST);
-	GameObject diamond("textures/diamond.png", GL_NEAREST);
+	GameObject diamond1("textures/diamond.png", GL_NEAREST, glm::vec3(0.0f,0.f,0.f), glm::vec3(0.5f,0.25f,0.f));
+	// GameObject diamond2("textures/diamond.png", GL_NEAREST, glm::vec3(0.0f,0.f,0.f), glm::vec3(0.5f,0.5f,0.f));
+	// GameObject diamond3("textures/diamond.png", GL_NEAREST, glm::vec3(0.0f,0.f,0.f), glm::vec3(0.5f,0.5f,0.f));
+	// GameObject diamond4("textures/diamond.png", GL_NEAREST, glm::vec3(0.0f,0.f,0.f), glm::vec3(0.5f,0.5f,0.f));
+	// GameObject diamond5("textures/diamond.png", GL_NEAREST, glm::vec3(0.0f,0.f,0.f), glm::vec3(0.5f,0.5f,0.f));
 
-	glm::mat4 T_ship = glm::translate(glm::mat4(1.f), glm::vec3(0.1f,0.1f,0.f));
-	glm::mat4 S_ship = glm::scale(glm::mat4(1.f), glm::vec3(0.18f, 0.18f, 0.f));
-	glm::mat4 R_ship = glm::rotate(glm::mat4(1.f), glm::radians(0.f), glm::vec3(0.f,0.f,1.f));
-	glm::mat4 M_ship = T_ship * R_ship * S_ship;
 
-	glm::mat4 T_diamond = glm::translate(glm::mat4(1.f), glm::vec3(-0.1f,-0.1f,0.f));
-	glm::mat4 S_diamond = glm::scale(glm::mat4(1.f), glm::vec3(0.18f, 0.18f, 0.f));
-	glm::mat4 R_diamond = glm::rotate(glm::mat4(1.f), glm::radians(50.f), glm::vec3(0.f,0.f,1.f));
-	glm::mat4 M_diamond = T_diamond * R_diamond * S_ship;
+	// std::vector<GameObject> uncollectedDiamonds = std::vector<GameObject>();
+	// uncollectedDiamonds.push_back(diamond1);
+	// uncollectedDiamonds.push_back(diamond2);
+	// uncollectedDiamonds.push_back(diamond3);
+	// uncollectedDiamonds.push_back(diamond4);
+	// uncollectedDiamonds.push_back(diamond5);
+
+	int score = 0;
+	bool start = false;
 
 	// RENDER LOOP
 	while (!window.shouldClose()) {
-		int score;
+		
 		Parameters user_input = callback->getParameters();
 
 		glfwPollEvents();
@@ -244,23 +256,26 @@ int main() {
 		glEnable(GL_FRAMEBUFFER_SRGB);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		// Get the user input details
-		float direction_change = user_input.direction;
-		glm::vec3 displacement_change = user_input.displacement;
+		// ship.updateShip(user_input, 0);
+		// // Get the user input details
+		// glm::vec2 cursorPos = user_input.cursorPos;
+		// glm::vec3 displacement_change = user_input.displacement;
 
-
-		// Recalculate ship position
+		// // Recalculate ship position (this is erroneous because it does not consider rotation #FIXME)
+		// ship.T_Matrix = glm::translate(glm::mat4(1.f), displacement_change);
+		// ship.position = displacement_change;
 		
 		ship.ggeom.setVerts(ship.cgeom.verts);
 		ship.ggeom.setTexCoords(ship.cgeom.texCoords);
-		diamond.ggeom.setVerts(diamond.cgeom.verts);
-		diamond.ggeom.setTexCoords(diamond.cgeom.texCoords);
+		diamond1.ggeom.setVerts(diamond1.cgeom.verts);
+		diamond1.ggeom.setTexCoords(diamond1.cgeom.texCoords);
 
 		// First render the ship
 		ship.ggeom.bind();
 		ship.texture.bind();
 
 		// Here go the transformations
+		glm::mat4 M_ship = ship.getTransformationMatrix();
 		glUniformMatrix4fv(0,1,GL_FALSE, glm::value_ptr(M_ship));
 		
 		// Draw ship then unbind texture
@@ -268,21 +283,21 @@ int main() {
 		
 		ship.texture.unbind();
 
-		// // Then render the diamond
-		diamond.ggeom.bind();
-		diamond.texture.bind();
+		// Then render the diamonds
+		diamond1.ggeom.bind();
+		diamond1.texture.bind();
+
+		diamond1.updateDiamondPosition();
 
 		// Here go the transformations
-		/*
-		
-		*/
+		glm::mat4 M_diamond = diamond1.getTransformationMatrix();
 		glUniformMatrix4fv(0,1,GL_FALSE, glm::value_ptr(M_diamond));
 
 		
 
 		// // Draw diamond then unbind texture
 		glDrawArrays(GL_TRIANGLES, 0, 6);
-		diamond.texture.unbind();
+		diamond1.texture.unbind();
 		
 		glDisable(GL_FRAMEBUFFER_SRGB); // disable sRGB for things like imgui
 
@@ -309,7 +324,7 @@ int main() {
 
 		// Scale up text a little, and set its value
 		ImGui::SetWindowFontScale(1.5f);
-		ImGui::Text("Score: %d", 0); // Second parameter gets passed into "%d"
+		ImGui::Text("Score: %d", score); // Second parameter gets passed into "%d"
 
 		// End the window.
 		ImGui::End();
