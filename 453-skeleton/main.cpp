599db48c3af973a9861893a0bfb8ccdb83907c48
Rfblaze <angelotroncone@gmail.com>
2024-11-26 22:04:14 -0700

    Rendered a unit sphere

--- 453-skeleton/main.cpp
+++ 453-skeleton/main.cpp
@@ -81,6 +81,40 @@ private:
 	double mouseOldY;
 };
 
+std::vector<glm::vec3> createSurfaceOfRevolution(const std::vector<glm::vec3>& CurvePoints, int numSlices) {
+    std::vector<glm::vec3> surfaceVertices;
+    float angleStep = glm::radians(360.0f / numSlices);
+
+    // For each point in the original curve
+    for (size_t i = 0; i < CurvePoints.size() - 1; ++i) {
+        glm::vec3 bottom1 = CurvePoints[i];
+        glm::vec3 bottom2 = CurvePoints[i + 1];
+
+        // Create triangles for each slice around the revolution axis
+        for (int slice = 0; slice < numSlices; ++slice) {
+            float angle1 = slice * angleStep;
+            float angle2 = (slice + 1) * angleStep;
+
+            // Rotate points around Y-axis
+            glm::vec3 p1(bottom1.x * cos(angle1), bottom1.y, bottom1.x * sin(angle1));
+            glm::vec3 p2(bottom1.x * cos(angle2), bottom1.y, bottom1.x * sin(angle2));
+            glm::vec3 p3(bottom2.x * cos(angle1), bottom2.y, bottom2.x * sin(angle1));
+            glm::vec3 p4(bottom2.x * cos(angle2), bottom2.y, bottom2.x * sin(angle2));
+
+            // First triangle of the quad
+            surfaceVertices.push_back(p1);
+            surfaceVertices.push_back(p2);
+            surfaceVertices.push_back(p3);
+
+            // Second triangle of the quad
+            surfaceVertices.push_back(p3);
+            surfaceVertices.push_back(p2);
+            surfaceVertices.push_back(p4);
+        }
+    }
+    return surfaceVertices;
+}
+
 int main() {
 	Log::debug("Starting main");
 
@@ -99,6 +133,39 @@ int main() {
 	UnitCube cube;
 	cube.generateGeometry();
 
+	float root2over2 = powf(2, 0.5) / 2.f;
+	float root3over2 = powf(3, 0.5) / 2.f;
+
+	std::vector<glm::vec3> sphereCoords = std::vector<glm::vec3>();
+	sphereCoords.push_back(glm::vec3(0.f,-1.f,0.f));
+	sphereCoords.push_back(glm::vec3(0.5f,-root3over2,0.f));
+	sphereCoords.push_back(glm::vec3(root2over2,-root2over2,0.f));
+	sphereCoords.push_back(glm::vec3(root3over2,-0.5f,0.f));
+	sphereCoords.push_back(glm::vec3(1.f,0.f,0.f));
+	sphereCoords.push_back(glm::vec3(root3over2,0.5f,0.f));
+	sphereCoords.push_back(glm::vec3(root2over2,root2over2,0.f));
+	sphereCoords.push_back(glm::vec3(0.5f,root3over2,0.f));
+	sphereCoords.push_back(glm::vec3(0.f,1.f,0.f));
+
+	std::vector<glm::vec3> Sphere3D = createSurfaceOfRevolution(sphereCoords, 16);
+
+	CPU_Geometry sphereCpu;
+	GPU_Geometry sphereGpu;
+
+	std::vector<glm::vec3> cols = std::vector<glm::vec3>();
+	for (int i = 0; i < Sphere3D.size(); i++){
+		cols.push_back(glm::vec3(0.f,0.f,0.f));
+	}
+
+	sphereCpu.verts = Sphere3D;
+	sphereCpu.cols = cols;
+	sphereCpu.normals = Sphere3D;
+
+	sphereGpu.setVerts(sphereCpu.verts);
+	sphereGpu.setCols(sphereCpu.cols);
+	sphereGpu.setNormals(sphereCpu.normals);
+
+
 	// RENDER LOOP
 	while (!window.shouldClose()) {
 		glfwPollEvents();
@@ -108,14 +175,20 @@ int main() {
 		glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glEnable(GL_DEPTH_TEST);
-		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL /*GL_LINE*/);
+		// glPolygonMode(GL_FRONT_AND_BACK, GL_FILL /*GL_LINE*/);
 
 		shader.use();
 
 		a4->viewPipeline(shader);
 
-		cube.m_gpu_geom.bind();
-		glDrawArrays(GL_TRIANGLES, 0, GLsizei(cube.m_size));
+		// cube.m_gpu_geom.bind();
+		// glDrawArrays(GL_TRIANGLES, 0, GLsizei(cube.m_size));
+
+		// My attempt at rendering
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+		sphereGpu.bind();
+		glDrawArrays(GL_TRIANGLES, 0, GLsizei(sphereCpu.verts.size()));
 
 		glDisable(GL_FRAMEBUFFER_SRGB); // disable sRGB for things like imgui
 		window.swapBuffers();
